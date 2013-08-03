a2345edf373e47fecad3f85f0b9a6b7228ab39fd
geraldoj8@gmail.com <geraldoj8@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-03 23:36:18 +0000

    Sprint 1 - Login - Criar tela Login Sprint 1 - Login - Método no botão "entrar" para home Sprint 1 - Verificação de login/senha no código

--- src/br/com/sccm/MainActivity.java
+++ src/br/com/sccm/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.sccm;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,41 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		
+		final EditText usuario = (EditText) findViewById(R.id.usuario);
+		final EditText senha = (EditText) findViewById(R.id.senha);
+		Button entrar = (Button) findViewById(R.id.entrar); 
+		
+		entrar.setOnClickListener(new View.OnClickListener() {
+			
+			
+			@Override
+			public void onClick(View v) {
+				
+				
+				
+				String logincerto = "admin";
+				String senhacerta = "admin";
+				String strLogin = usuario.getText().toString();
+				String strSenha = senha.getText().toString();
+				
+				if (strLogin.equals(logincerto) && strSenha.equals(senhacerta)){
+					Toast.makeText(MainActivity.this, "Login com Sucesso!", Toast.LENGTH_SHORT).show();
+					chamaHome();
+				}else{
+					Toast.makeText(MainActivity.this, "Login e/ou Senha Incorretos!", Toast.LENGTH_SHORT).show();
+					
+				}
+				
+			}
+		});
+
+
+	}
+	public void chamaHome(){
+		setContentView(R.layout.home);
 	}
 
 	@Override
