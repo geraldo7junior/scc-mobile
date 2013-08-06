7ab230997880283b8adeca8536f845e3e424d1ae
ulrichugo@gmail.com <ulrichugo@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-06 18:01:19 +0000

    Sprint 2 - Integração de códigos e pendências até o dia 05/08/13

---
+++ src/br/com/sccm/Apontamentos.java
@@ -0,0 +1,12 @@
+package br.com.sccm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Apontamentos extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.apontamentos);}
+
+}
