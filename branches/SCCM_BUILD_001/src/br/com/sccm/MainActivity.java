5a1641dd3477bcf6285679d037b7c616de6cec78
aldo1ror@gmail.com <aldo1ror@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-06 21:28:14 +0000

    Sprint 1 - CM - Criação da build referente à entrega do dia 06/08/13

---
+++ branches/SCCM_BUILD_001/src/br/com/sccm/MainActivity.java
@@ -0,0 +1,83 @@
+package br.com.sccm;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		
+		final EditText usuario = (EditText) findViewById(R.id.usuario);
+		final EditText senha = (EditText) findViewById(R.id.senha);
+		Button entrar = (Button) findViewById(R.id.entrar); 
+		
+		usuario.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				usuario.setText("");
+			}
+		});
+		
+		senha.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				senha.setText("");
+			}
+		});
+		
+		entrar.setOnClickListener(new View.OnClickListener() {
+			
+			
+			@Override
+			public void onClick(View v) {
+				
+				
+				
+				String logincerto = "admin";
+				String senhacerta = "admin";
+				String strLogin = usuario.getText().toString();
+				String strSenha = senha.getText().toString();
+				
+				if (strLogin.equals(logincerto) && strSenha.equals(senhacerta)){
+					Toast.makeText(MainActivity.this, "Login com Sucesso!", Toast.LENGTH_SHORT).show();
+					chamaHome();
+				}else{
+					Toast.makeText(MainActivity.this, "Login e/ou Senha Incorretos!", Toast.LENGTH_SHORT).show();
+					
+				}
+				
+			}
+		});
+		
+		
+
+
+
+	}
+	public void chamaHome(){
+		setContentView(R.layout.home);
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
