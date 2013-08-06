ce64b6edad388592077309a00abf9feb22dd1187
ulrichugo@gmail.com <ulrichugo@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-06 18:01:19 +0000

    Sprint 2 - Integração de códigos e pendências até o dia 05/08/13

--- trunk/gen/br/com/sccm/R.java
+++ trunk/gen/br/com/sccm/R.java
@@ -10,6 +10,9 @@ package br.com.sccm;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int pressed_sccm=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,35 +20,111 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int background1=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int logo=0x7f020002;
+        public static final int ab_background_textured_sccm=0x7f020000;
+        public static final int ab_bottom_solid_sccm=0x7f020001;
+        public static final int ab_solid_sccm=0x7f020002;
+        public static final int ab_stacked_solid_sccm=0x7f020003;
+        public static final int ab_texture_tile_sccm=0x7f020004;
+        public static final int ab_transparent_sccm=0x7f020005;
+        public static final int agendaico=0x7f020006;
+        public static final int apontamentosico=0x7f020007;
+        public static final int background1=0x7f020008;
+        public static final int btn_cab_done_default_sccm=0x7f020009;
+        public static final int btn_cab_done_focused_sccm=0x7f02000a;
+        public static final int btn_cab_done_pressed_sccm=0x7f02000b;
+        public static final int btn_cab_done_sccm=0x7f02000c;
+        public static final int btnentrar=0x7f02000d;
+        public static final int cab_background_bottom_sccm=0x7f02000e;
+        public static final int cab_background_top_sccm=0x7f02000f;
+        public static final int consultoresico=0x7f020010;
+        public static final int empresasico=0x7f020011;
+        public static final int ic_launcher=0x7f020012;
+        public static final int list_focused_sccm=0x7f020013;
+        public static final int logo=0x7f020014;
+        public static final int menu_dropdown_panel_sccm=0x7f020015;
+        public static final int pressed_background_sccm=0x7f020016;
+        public static final int progress_bg_sccm=0x7f020017;
+        public static final int progress_horizontal_sccm=0x7f020018;
+        public static final int progress_primary_sccm=0x7f020019;
+        public static final int progress_secondary_sccm=0x7f02001a;
+        public static final int projetosico=0x7f02001b;
+        public static final int selectable_background_sccm=0x7f02001c;
+        public static final int spinner_ab_default_sccm=0x7f02001d;
+        public static final int spinner_ab_disabled_sccm=0x7f02001e;
+        public static final int spinner_ab_focused_sccm=0x7f02001f;
+        public static final int spinner_ab_pressed_sccm=0x7f020020;
+        public static final int spinner_background_ab_sccm=0x7f020021;
+        public static final int tab_indicator_ab_sccm=0x7f020022;
+        public static final int tab_selected_focused_sccm=0x7f020023;
+        public static final int tab_selected_pressed_sccm=0x7f020024;
+        public static final int tab_selected_sccm=0x7f020025;
+        public static final int tab_unselected_focused_sccm=0x7f020026;
+        public static final int tab_unselected_pressed_sccm=0x7f020027;
+        public static final int tab_unselected_sccm=0x7f020028;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int entrar=0x7f080002;
-        public static final int imageView1=0x7f080003;
-        public static final int senha=0x7f080001;
-        public static final int usuario=0x7f080000;
+        public static final int action_settings=0x7f090015;
+        public static final int btnapontamentos=0x7f090014;
+        public static final int editText1=0x7f090006;
+        public static final int editText2=0x7f090007;
+        public static final int entrar=0x7f090003;
+        public static final int imageView1=0x7f090002;
+        public static final int imageView2=0x7f090010;
+        public static final int imageView3=0x7f090012;
+        public static final int imageView5=0x7f090011;
+        public static final int imageView6=0x7f090013;
+        public static final int radio0=0x7f09000b;
+        public static final int radio1=0x7f090008;
+        public static final int radio2=0x7f09000c;
+        public static final int radioButton1=0x7f09000f;
+        public static final int radioButton2=0x7f09000e;
+        public static final int radioGroup1=0x7f090005;
+        public static final int senha=0x7f090001;
+        public static final int textView1=0x7f090004;
+        public static final int textView2=0x7f090009;
+        public static final int textView3=0x7f09000a;
+        public static final int textView4=0x7f09000d;
+        public static final int usuario=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int home=0x7f030001;
+        public static final int apontamentos=0x7f030001;
+        public static final int home=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int Entrar=0x7f050002;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int logo=0x7f050003;
+        public static final int A=0x7f06000f;
+        public static final int B=0x7f060010;
+        public static final int C=0x7f060011;
+        public static final int Entrar=0x7f060002;
+        public static final int Senha=0x7f060005;
+        public static final int TipoHora=0x7f06000d;
+        public static final int Tipoconsultoria=0x7f06000e;
+        public static final int Titulo=0x7f06000b;
+        public static final int Usuario=0x7f060004;
+        public static final int action_settings=0x7f060001;
+        public static final int agendaico=0x7f06000a;
+        public static final int apontamentosico=0x7f060009;
+        public static final int app_name=0x7f060000;
+        public static final int consultoresico=0x7f060006;
+        public static final int data=0x7f06000c;
+        public static final int empresasico=0x7f060008;
+        public static final int grupo=0x7f060013;
+        public static final int individual=0x7f060012;
+        public static final int logo=0x7f060003;
+        public static final int projetosico=0x7f060007;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Sccm=0x7f070003;
+        public static final int ActionBar_Transparent_Sccm=0x7f070004;
+        public static final int ActionBarTabStyle_Sccm=0x7f070007;
+        public static final int ActionButton_CloseMode_Sccm=0x7f07000a;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -67,10 +146,18 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int DropDownListView_Sccm=0x7f070006;
+        public static final int DropDownNav_Sccm=0x7f070008;
+        public static final int PopupMenu_Sccm=0x7f070005;
+        public static final int ProgressBar_Sccm=0x7f070009;
+        public static final int Theme_Sccm=0x7f070002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Sccm_Widget=0x7f07000b;
     }
 }
--- trunk/src/br/com/sccm/MainActivity.java
+++ trunk/src/br/com/sccm/MainActivity.java
@@ -1,13 +1,7 @@
 package br.com.sccm;
 
-import br.com.sccm.database.Activities;
-import br.com.sccm.database.Addresses;
-import android.database.sqlite.SQLiteDatabase; //armazena as informações dentro do banco de dados
-import android.database.Cursor; //Manipula os dados
-import android.R;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog; //
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -20,13 +14,30 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		openOrCreateDatabase(); //chamada para o método de abertura do banco
 		
 		
 		
 		final EditText usuario = (EditText) findViewById(R.id.usuario);
 		final EditText senha = (EditText) findViewById(R.id.senha);
 		Button entrar = (Button) findViewById(R.id.entrar); 
+		
+		usuario.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				usuario.setText("");
+			}
+		});
+		
+		senha.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				senha.setText("");
+			}
+		});
 		
 		entrar.setOnClickListener(new View.OnClickListener() {
 			
@@ -51,12 +62,16 @@ public class MainActivity extends Activity {
 				
 			}
 		});
+		
+		
+
 
 
 	}
 	public void chamaHome(){
 		setContentView(R.layout.home);
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,162 +79,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	//Banco de dados SQLite
-	
 
-	
-	SQLiteDatabase Database = null;
-	Cursor Cursor;
-	String Name = "swsdb";
-	
-	public void messages (String Title, String Text) {
-		
-		AlertDialog.Builder message = new AlertDialog.Builder(MainActivity.this);
-		message.setTitle(Title);
-		message.setMessage(Text);
-		message.setNeutralButton("OK", null);
-		message.show();
-		
-		
-	}
-	
-	//método para abertura ou criação dos banco de dados
-	
-	public void openOrCreateDatabase () {
-		
-		try {
-			//Criação do banco de dados
-			SQLiteDatabase.openOrCreateDatabase(Name, null );
-			
-			//Início da criação das tabelas
-			String Activities = "Create if not exists table  Activities("+ 
-	                " _id integer primary key auto_increment, not null" +
-	                " start_hours  text time(timestring, modifier, modifier) not null" +
-	                " end_hours  text time (timestring, modifier, modifier) not null"+
-	                " Date text date (timestring, modifier, modifier) not null"+
-	                " Observations text not null"+
-	                " Description  text not null"+
-	                " Status text not null" +
-	                " Consultant1_id integer not null"+
-	                " Consultant2_id integer not null"+
-	                " Consultant3_id integer not null"+
-	                " Consultant4_id integer not null "+
-	                " Removed Integer not null ";
-			Database.execSQL(Activities);
-			
-			String Consultants = "CREATE TABLE IF NOT EXISTS Consultants (_id PRIMARY KEY NOT NULL AUTO_INCREMENT, Cpf "+
-					"INTEGER NOT NULL, Name TEXT NOT NULL, Acronym TEXT NOT NULL, Acronym_color"+ 
-					"TEXT NOT NULL, Phone1  INTEGER NOT NULL, Phone2 INTEGER NOT NULL, Email"+ 
-					"TEXT NOT NULL, Removed INTEGER NOT NULL, Photo BLOB NOT NULL )";
-			Database.execSQL(Consultants);
-			
-			String Activities_consultants = "Create if not exists Activities_consultants (_id  INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, Consultant_id"+ 
-					"INTEGER NOT NULL, Activity_id INTEGER NOT NULL CONSTRAINT,Activity_id references Activities (_id), CONSTRAINT Consultant_id " +
-					"references Consultants (_id))";
-			Database.execSQL(Activities_consultants);
-			
-			String Addresses = "Create TABLE IF NOT EXISTS Addresses (_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,Address" + 
-				"TEXT NOT NULL,  Number  TEXT, Neighborhood TEXT, City  TEXT, State  TEXT, Consultant_id" + 
-				"INTEGER NOT NULL, Company_id  INTEGER NOT NULL, CONSTRAINT Consultant_id references Consultants (_id)";
-			Database.execSQL(Addresses);
-			
-			String Attachments = " CREATE TABLE IF NOT EXISTS Attachments (_id INTEGER PRIMARY KEY AUTO_INCREMENT" +
-				" Name  TEXT, Email TEXT, Phone INTEGER )";
-			Database.execSQL(Attachments);
-			
-			String Owners = "CREATE TABLE IF NOT EXISTS Owners (_id INTEGER PRIMARY KEY AUTO_INCREMENT, Name TEXT," +
-					"Email TEXT, PHONE TEXT, DATE TEXT)";
-			Database.execSQL(Owners);
-			
-			String Companies = "CREATE IF NOT EXISTS TABLE Companies (_id INTEGER PRIMAY KEY AUTO_INCREMENT NOT NULL, Cnpj"+
-				" INTEGER NOT NULL, Name TEXT NOT NULL, Acronym TEXT NOT NULL, Phone1 INTEGER, Phone2"+
-				" INTEGER, Logo  TEXT, Removed  TEXT, Fundation TEXT, Owner_id INTEGER)";
-			Database.execSQL(Companies);
-			
-			String Companies_bank_infos = "CREATE IF NOT EXISTS TABLE Companies_bank_infos  (_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, Name_bank"+
-				" TEXT, Number_agency INTEGER, Number_account INTEGER, Company_id INTEGER, CONSTRAINT Company_id REFERENCES Companies (_id)"; 
-			Database.execSQL( Companies_bank_infos);
-			
-			String Companies_contacts = "CREATE IF NOT EXISTS TABLE Companies_contacts(_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, Name"+
-    			" TEXT, Email TEXT, Function TEXT, Telephone INTEGER,  Company_id"+ 
-    			" INTEGER, CONSTRAINT company_ id REFERENCES companies(_id)";
-			Database.execSQL( Companies_contacts);
-			
-			String Consultants_bank_infos = "CREATE IF NOT EXISTS TABLE Consultants_bank_infos  (_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, Name_bank"+
-					" TEXT, Number_agency INTEGER, Number_account INTEGER, Consultant_id INTEGER, CONSTRAINT Consultant_id REFERENCES consultants(_id)"; 
-			Database.execSQL( Consultants_bank_infos);
-			
-			String Entries = "CREATE IF NOT EXISTS TABLE Entries  ( _id INTEGER PRIMARY KEY AUTO_INCREMENTNOT NULL, Date"+ 
-				" TEXT NOT NULL, Type_consulting TEXT NOT NULL, Type TEXT NOT NULL Hours_worked TEXT NOT NULL," +
-						" Observations TEXT, Consultant_id INTEGER NOT NULL, Activity_id INTEGER NOT NULL, Approved"+ 
-						"INTEGER NOT NULL, Removed INTEGER NOT NULL, CONSTRAINT Activity_id REFERENCES Activities(_id), CONSTRAINT Consultant_id REFERENCES Consultants(_id))";
-			
-			Database.execSQL(Entries);
-				
-			String Projects = "CREATE IF NOT EXISTS TABLE Projects (_id INTEGER NOT NULL PRIMARY_KEY AUTO_INCREMENT, Name"+
- 				" TEXT NOT NULL, Description TEXT NOT NULL, Acronym TEXT NOT NULL, A_hours_individual REAL, B_hours_individual REAL, C_hours_individual, REAL, A_hours_group"+ 
- 				"REAL, B_hours_group REAL, C_hours_group REAL, Consultant_id  INTEGER, Parent_project_id INTEGER, Company_id"+
- 				" INTEGER NOT NULL, Removed INTEGER NOT NULL, CONSTRAINT Company_id REFERENCES Companies(_id), CONSTRAINT Consultant_id REFERENCES Consultants(_id)";
- 		   
-			Database.execSQL(Projects);
- 		   
- 		   String Expenses = "CREATE IF NOT EXISTS TABLE Expenses" +
-					"(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, Description"+ 
-					"TEXT NOT NULL, Value REAL NOT NULL, Type  TEXT, Project_id"+ 
-					" INTEGER NOT NULL, Attachments BLOB, File" +
-					"BLOB, Type_expenses TEXT, CONSTRAINT Project_id references Projects (_id))";
- 		   
- 		   Database.execSQL(Expenses);
- 		   
- 		   String Financials = "CREATE IF NOT EXISTS TABLE Financials ( _id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, Name"+
- 	    			" TEXT, Phone1 TEXT, Phone2 TEXT, Email TEXT, Company_id INTEGER, CONSTRAINT Company_id REFERENCES COMPANIES (_id))";
-
- 		  Database.execSQL(Financials);
- 		  
- 		   String Projects_Consultants = " _id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,  Project_id INTEGER NOT NULL"+
- 				                         "Project_id INTEGER NOT NULL, Value_hour_a_individual REAL,Value_hour_b_individual REAL" +
- 				                         "Value_hour_c_individual REAL,Value_hour_a_group REAL, Value_hour_b_group REAL, Value_hour_c_group REAL," +
- 				                         "CONSTRAINT Consultant_id REFERENCES Consultants(_id), CONSTRAINT Project_id REFERENCES Projects(_id))";
-  		  Database.execSQL(Projects_Consultants);
-  		  
-  		  
-  		  String Sepgs = "CREATE IF NOT EXISTS TABLE Segps(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, Name TEXT, Phone1 TEXT" +
-  		                 "Phone2 TEXT, EMAIL TEXT, Company_id INTEGER, CONSTRAINT Company_id REFERENCES Companies (_id))";
-                         
-                         
-  		  Database.execSQL(Sepgs);
-  		  
-  		  String Sponsors = "CREATE IF NOT EXISTS TABLE Segps(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, Name TEXT, Phone1 TEXT" +
-	                 "Phone2 TEXT, EMAIL TEXT, Company_id INTEGER, CONSTRAINT Company_id REFERENCES Companies (_id))";
-                  
-			
-			
-		 String Users = "CREATE IF NOT EXISTS TABLE Segps(_id INTEGER NOT NULLAUTO_INCREMENT, Username TEXT PRIMARY KEY," +
-		 		"Password TEXT, Type TEXT, Consultant_id INTEGER";
-		 
-		 //Fim da criação das tabelas
-		 Database.execSQL(Users);
-		 messages("Sucesso!","Banco de dados criado corretamente");
-				
-		}catch (Exception Erro) {
-			messages("Erro!","Falha ao criar ou abrir o banco de dados!"+Erro.getMessage());
-				}
-	
-		}
-
-		
-	
-	public void closeDatabase () {
-		
-		try {
-			Database.close();
-			messages ("Sucesso!","Banco de dados finalizado corretamente.");
-			
-		}catch (Exception Erro) {
-			messages ("Erro!","Falha ao finalizar banco de dados!"+Erro.getMessage());
-			
-		}
-	}
-	
 }
