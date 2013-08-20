44edcd5c44d26bb5db2c6d18583225bd40350b3a
humbertobsjunior@gmail.com <humbertobsjunior@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-20 20:42:22 +0000

    Sprint 4 - CM - Reestruturação do projeto

---
+++ trunk/src/br/com/sccm/Home.java
@@ -0,0 +1,33 @@
+package br.com.sccm;
+
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+
+
+public class Home extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+	}
+	
+	//#################ROBSON##############
+	public void loadPage(){
+		setContentView(R.layout.apontamentos);
+	}
+	//####################FIM##############
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+
---
+++ trunk/verify/login.php
@@ -0,0 +1,19 @@
+<?php
+	//$username=$_GET['username'];
+	//$password=$_GET['password'];
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$conect = mysql_connect('localhost','root','');
+	mysql_select_db('swsdb',$conect);
+	$sql="select * from users where username='$username' and password='$password'";
+	$result = mysql_query($sql) or die ("Erro. :" . mysql_error());
+	
+	if (mysql_num_rows($result)>0){
+		echo '1';
+		
+	}
+	else{
+		echo '0';
+		
+	}
+?>
