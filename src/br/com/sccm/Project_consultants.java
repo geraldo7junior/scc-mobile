88682ddb96d796c219772f2f813cfc8ef96640cd
jrjcsj@gmail.com <jrjcsj@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-27 04:56:04 +0000

    Sprint 4 - Integração da classe da tabela Project_consultants com o código reestruturado.

---
+++ src/br/com/sccm/Project_consultants.java
@@ -0,0 +1,16 @@
+package br.com.sccm;
+
+public class Project_consultants {
+	
+	public static final String Project_id = "Project_id";
+	public static final String Consultant_id = "Consultant_id";
+	
+	
+	public static final String CreateTableProject_consultants = "CREATE TABLE IF NOT EXISTS Project_consultants (" +
+			                                                     Project_id+" INTEGER NOT NULL, "+Consultant_id+" " +
+			                                                     "INTEGER NOT NULL CONSTRAINT "+Project_id+" REFERENCES " +
+			                                                     "Id (Projects) CONSTRAINT "+Consultant_id+" REFERENCES " +
+			                                                     "Id (Consultants));";
+	
+
+}
