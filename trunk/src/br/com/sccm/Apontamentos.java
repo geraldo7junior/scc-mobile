71e0d00e4f3b51f4374405e7d646b1852345c9c4
ulrichugo@gmail.com <ulrichugo@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-27 16:07:40 +0000

    Sprint 5 - Merge conexão externa e janelas

--- trunk/src/br/com/sccm/Apontamentos.java
+++ trunk/src/br/com/sccm/Apontamentos.java
@@ -2,11 +2,30 @@ package br.com.sccm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Apontamentos extends Activity {
+	Spinner sp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.apontamentos);}
+		setContentView(R.layout.apontamentos);
+		
+		sp=(Spinner) findViewById(R.id.spinner1);
+		
+		ArrayAdapter<CharSequence> ar= ArrayAdapter.createFromResource(this, R.array.projeto, android.R.layout.simple_list_item_1);
+		
+		ar.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		sp.setAdapter(ar);
+		
+		sp=(Spinner) findViewById(R.id.spinner2);
+		
+		ArrayAdapter<CharSequence> arr= ArrayAdapter.createFromResource(this, R.array.atividades, android.R.layout.simple_list_item_1);
+		
+		arr.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		sp.setAdapter(arr);
+		
+	}
 
 }
