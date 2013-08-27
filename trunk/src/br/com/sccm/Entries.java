d22f10749869f8392f40967de53747fd5319bf5b
jrjcsj@gmail.com <jrjcsj@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-08-27 04:54:35 +0000

    Sprint 4 - Integração da classe da tabela Entries com o código reestruturado.

---
+++ trunk/src/br/com/sccm/Entries.java
@@ -0,0 +1,26 @@
+package br.com.sccm;
+
+public class Entries {
+	public static final String Id = "id";
+	public static final String Title = "title";
+	public static final String Date = "date";
+	public static final String Type_hour = "type_hour";
+	public static final String Type_consultancy = "type_consultancy";
+	public static final String Consultant_id = "consultant_id";
+	public static final String Activity_id = "Activity_id";
+	
+	
+	
+	public static final String CreateTableEntries = "CREATE IF NOT EXISTS TABLE Entries ("+
+								  Id+" INTEGER PRIMARY_KEY, "+
+								  Title+" TEXT, "+
+								  Date+" TEXT, "+
+								  Type_hour+" TEXT, "+
+								  Type_consultancy+" TEXT, "+
+								  Consultant_id+" TEXT, "+
+								  Activity_id+" TEXT) "+
+								  "CONSTRAINT Consultant_id REFERENCES Id(Consultants)" +
+								  "CONSTRAINT Activity_id REFERENCES Id(Activities)";
+	
+
+}
