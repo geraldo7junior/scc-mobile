82447a31c4041a7db275aba27eda0e48de16b03c
jrjcsj@gmail.com <jrjcsj@gmail.com@35e7efa6-8a4e-e014-df3f-3c61ec58253d>
2013-09-16 21:29:26 +0000

    Sprint Final - Alterações na tabela Entries - Cadastrar apontamento no banco

--- src/br/com/sccm/db/Entries.java
+++ src/br/com/sccm/db/Entries.java
@@ -1,26 +1,30 @@
-package br.com.sccm.db;
+package br.com.sccm;
 
 public class Entries {
 	public static final String Id = "id";
+	public static final String Project_name = "project_name";
+	public static final String Activity_name = "activity_name";
 	public static final String Title = "title";
 	public static final String Date = "date";
+	public static final String Hour = "hour";
 	public static final String Type_hour = "type_hour";
 	public static final String Type_consultancy = "type_consultancy";
-	public static final String Consultant_id = "consultant_id";
-	public static final String Activity_id = "Activity_id";
+
 	
 	
 	
-	public static final String CreateTableEntries = "CREATE IF NOT EXISTS TABLE Entries ("+
+	public static final String CreateTableEntries = "CREATE TABLE Entr ("+
 								  Id+" INTEGER PRIMARY_KEY, "+
+								  Project_name+" TEXT, "+
+								  Activity_name+" TEXT, "+
 								  Title+" TEXT, "+
 								  Date+" TEXT, "+
+								  Hour+" TEXT, "+
 								  Type_hour+" TEXT, "+
-								  Type_consultancy+" TEXT, "+
-								  Consultant_id+" TEXT, "+
-								  Activity_id+" TEXT) "+
-								  "CONSTRAINT Consultant_id REFERENCES Id(Consultants)" +
-								  "CONSTRAINT Activity_id REFERENCES Id(Activities)";
+								  Type_consultancy+" TEXT)";
+								  
+								//  "CONSTRAINT Consultant_id REFERENCES Id(Consultants)" +
+								//  "CONSTRAINT Activity_id REFERENCES Id(Activities)";
 	
 
 }
